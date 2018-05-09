2d8d3e4a8d29d46c83ac37d812335f332d5798b7
Anolog <steve.kostiw@gmail.com>
2018-05-09 17:58:11 -0400

    Created achievement manager and achievement

---
+++ GameFramework/Assets/Scripts/Achievement.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+
+// <T> is the variable type you want to be tracked, i.e., bool, int, float...
+public class Achievement
+{
+    //Achievement variables
+    private string m_AchievementName;
+    private string m_AchievementDescription;
+    private int m_AchievementPoints;
+    private bool m_AchievementEarned;
+    private DateTime m_TimeAchievementEarned;
+
+    private object m_CurrentAchievementProgress;
+    private object m_RequiredToEarnAchievement;
+
+    Achievement(string aAchievementName, string aAchievementDescription, int aAchievementPoints, object aRequiredToEarnAchievement)
+    {
+        m_AchievementName = aAchievementName;
+        m_AchievementDescription = aAchievementDescription;
+        m_AchievementPoints = aAchievementPoints;
+        m_AchievementEarned = false;
+        m_RequiredToEarnAchievement = aRequiredToEarnAchievement;
+
+        //Not sure if this will work, but 0 is false, 0 is an int, but 0.0 for float as well...?
+        m_CurrentAchievementProgress = 0;
+        
+    }
+
+    //Getters for the information for the achievements
+
+    public string GetAchievementName()
+    {
+        return m_AchievementName;
+    }
+
+    public string GetAchievementDescription()
+    {
+        return m_AchievementDescription;
+    }
+
+    public int GetAchievementPoints()
+    {
+        return m_AchievementPoints;
+    }
+
+    public bool GetAchievementEarned()
+    {
+        return m_AchievementEarned;
+    }
+
+    public DateTime GetTimeAchievementEarned()
+    {
+        return m_TimeAchievementEarned;
+    }
+
+    public object GetRequiredToEarnAchievement()
+    {
+        return m_RequiredToEarnAchievement;
+    }
+
+    public object GetCurrentProgress()
+    {
+        return m_CurrentAchievementProgress;
+    }
+    
+    //Needs a setter cause it will update semi-regularly
+    public void SetCurrentProgress(object aCurrentAchievementProgress)
+    {
+        m_CurrentAchievementProgress = aCurrentAchievementProgress;
+    }
+}
---
+++ GameFramework/Assets/Scripts/AchievementManager.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class AchievementManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<Achievement> m_ListOfAchievements;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void AchievementEarnedInterface(Achievement aAchievement, bool aFadeInterface)
+    {
+        //Call interface manager functions
+    }
+
+    public void AddAchievementToList(Achievement aAchievementToAdd)
+    {
+        m_ListOfAchievements.Add(aAchievementToAdd);
+    }
+
+    public List<Achievement> GetListOfAchievements()
+    {
+        return m_ListOfAchievements;
+    }
+
+    public float CheckAchievementStatus(int aIndex)
+    {
+        //Will need to test if this works vvvvvvvv
+        //if (m_ListOfAchievements[aIndex].GetCurrentProgress().GetType() == typeof(int))
+        try
+        {
+            return ( (float) (m_ListOfAchievements[aIndex].GetCurrentProgress() ) ) / 
+                   ( (float) (m_ListOfAchievements[aIndex].GetRequiredToEarnAchievement() ) );
+        }
+
+        catch(IndexOutOfRangeException e)
+        {
+            throw new ArgumentOutOfRangeException("Index " + aIndex + " is out of range\n" + e.Message);
+        }
+    }
+
+    public float CheckAchievementStatus(string aAchievementName)
+    {
+        Achievement rangeCheck = null;
+        float returnValue = -1;
+        
+        //Not sure if this will work...
+
+        try
+        {
+            foreach (Achievement achievement in m_ListOfAchievements)
+            {
+                if (achievement.GetAchievementName() == aAchievementName)
+                {
+                    rangeCheck = achievement;
+
+                    returnValue = ((float)(achievement.GetCurrentProgress())) /
+                                  ((float)(achievement.GetRequiredToEarnAchievement()));
+                    break;
+                }
+            }
+
+            return returnValue;
+        }
+
+        catch (ArgumentNullException e)
+        {
+            throw new ArgumentNullException("Achievement Name: " + aAchievementName + " was not found in m_ListOfAchievements\n" + e.Message);
+        }
+    }
+
+    public Achievement GetAchievementFromList(int aIndex)
+    {
+        try
+        {
+            return m_ListOfAchievements[aIndex];
+        }
+
+        catch (IndexOutOfRangeException e)
+        {
+            throw new ArgumentOutOfRangeException("Index " + aIndex + " is bigger than m_ListOfAchievements.Count " + m_ListOfAchievements.Count + "\n" + e.Message);
+        }
+
+    }
+
+    public Achievement GetAchievementFromList(string aAchievementName)
+    {
+        Achievement returnValue = null;
+
+        try
+        {
+            foreach (Achievement achievement in m_ListOfAchievements)
+            {
+                if (achievement.GetAchievementName() == aAchievementName)
+                {
+                    returnValue = achievement;
+                    break;
+                    
+                }
+            }
+
+            return returnValue;
+
+        }
+
+        catch(ArgumentNullException e)
+        {
+            throw new ArgumentNullException("Achievement Name: " + aAchievementName + " was not found in m_ListOfAchievements\n" + e.Message);
+        }
+    }
+}

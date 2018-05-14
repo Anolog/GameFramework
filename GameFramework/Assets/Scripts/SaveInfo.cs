a394c42903996a33625222c8629cde7de163c908
GriffynWilkinson <38253161+GriffynWilkinson@users.noreply.github.com>
2018-05-14 09:06:07 -0400

    Started work on the SaveManager

---
+++ GameFramework/Assets/Scripts/SaveInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveInfo {
+
+    private string m_SaveFile; //just using a string for now until we decide how we want this to work
+
+    public void SetSaveInfo(string aSaveInfo) { m_SaveFile = aSaveInfo; }
+
+    // this function will do more in the future, but for now it will just return the save info
+    public string ReadSave()
+    {
+        return m_SaveFile;
+    }
+}
---
+++ GameFramework/Assets/Scripts/SaveManager.cs
@@ -0,0 +1,99 @@
+﻿
+
+public class SaveManager {
+
+    //Variables
+    private SaveInfo[] m_SaveSlots = new SaveInfo[m_AmountOfSaveSlots];
+    private const int m_AmountOfSaveSlots = 10; // this number can be changed
+    private int m_CurrentSave;
+    private SaveInfo m_CurrentSaveInfo;
+    private string m_SaveDefaultFilePath;
+    private string m_SaveCustomFilePath;
+    private string m_CurrentSaveFilePath;
+
+    //Methods
+    public SaveInfo[] GetSaveSlots()
+    {
+        return m_SaveSlots;
+    }
+
+    //Will save the current save to the desired save slot
+    public void SaveCurrentSlot(int aSaveSlot)
+    {
+
+    }
+
+    //takes index of save slot and erases the data in it
+    public void EraseSaveSlot(int aSaveToDelete)
+    {
+        if (aSaveToDelete < m_AmountOfSaveSlots)
+        {
+            m_SaveSlots[aSaveToDelete] = null;
+        }
+        else
+        {
+            throw new System.Exception("Save slot you are trying to delete does not exist");
+        }
+    }
+
+    public void EraseAllSaveSlots()
+    {
+        for (int i = 0; i < m_AmountOfSaveSlots; ++i)
+        {
+            EraseSaveSlot(i);
+        }
+    }
+
+    public void DuplicateSaveSlot(int aSaveSlotToDuplicate, int aOtherSaveSlot)
+    {
+        // this function will likely have to do more once the save info is completed
+        m_SaveSlots[aOtherSaveSlot] = m_SaveSlots[aSaveSlotToDuplicate];
+    }
+
+    public int GetAmountOfSaves()
+    {
+        int counter = 0;
+        for (int i = 0; i < m_AmountOfSaveSlots; ++i)
+        {
+            if (m_SaveSlots[i] != null)
+            {
+                counter++;
+            }
+        }
+
+        return counter;
+    }
+
+    // This will load the save information into the game
+    // Can be overwritten for game specific information
+    // Currently doesn't do anything
+    public virtual void LoadSaveSlot()
+    {
+
+    }
+
+    // this will send the save slots to the database
+    // currently does nothing
+    public void UploadSaveSlotsToDatabase()
+    {
+
+    }
+
+    // will get save info from the database and makes it equal to save slots
+    public void RecoverSaveSlotsFromDatabase()
+    {
+
+    }
+
+    // Changes the current save file path to the one passed in
+    public void ChangeSaveDirectory(string aNewSaveFilePath)
+    {
+        m_CurrentSaveFilePath = aNewSaveFilePath;
+    }
+
+    // Resets the save file path to default
+    public void ResetSaveFilePath()
+    {
+        m_CurrentSaveFilePath = m_SaveDefaultFilePath;
+    }
+}
